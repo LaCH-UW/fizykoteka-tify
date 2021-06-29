d65db6045d35fd1998b4a4845ccccf59fda77fa1
mw0000 <m@wilkowski.org>
2021-06-29 13:37:29 +0200

    item metadata from API

--- src/components/____MetadataList.vue
+++ src/components/____MetadataList.vue
@@ -1,18 +1,73 @@
 <template>
-  <div class="tify-info_metadata">
-    <div v-for="(f, index) in apiMetadata" :key="index">
-      <template v-if="f.field_name !== 'Tytuł'">
-        <h4>{{ f.field_name }}</h4>
 
-        <div class="tify-info_content">
-          <span v-html="f.html"></span>
-        </div>
-      </template>
-    </div>
-  </div>
+	<div class="tify-info_metadata" v-if="null">
+		<div v-for="(item, index) in meta" :key="index">
+
+			<template v-if="item.label !== 'Tytuł'">
+
+					<h4>{{item.label}} [{{item.order}}]</h4>
+
+					<div class="tify-info_content">
+						<div
+						class="tify-info_value"
+						:class="{ '-collapsed': infoItems && infoItems[index] && infoItems[index].isCollapsed }"
+						:style="infoItems && infoItems[index] && infoItems[index].isCollapsed ? collapsedStyle : null"
+						v-bind:key="v" v-for="v in getLabels(item.value)"
+						>
+						<!-- tutaj values musza isc przez getLabels!!
+						w search wyszukiwanie po label
+						a moze dodac widok metadane/forma/dfsdfsdf?
+						-->
+
+							<a :href="'/szukaj/' + item.label + '/' + v" v-html="v" />
+
+						</div>
+					</div>
+
+			</template>
+
+			<template v-if="item.label !== 'Tytuł' && null">
+				<h4>
+					<div v-bind:key="index" v-for="(label, index) in getLabels(item.label)">
+						{{ label|cleanLabel }}
+					</div>
+				</h4>
+				<div class="tify-info_content">
+					<div
+						class="tify-info_value"
+						:class="{ '-collapsed': infoItems && infoItems[index] && infoItems[index].isCollapsed }"
+						:style="infoItems && infoItems[index] && infoItems[index].isCollapsed ? collapsedStyle : null"
+					>
+						<div v-bind:key="i" v-for="(value, i) in getLabels(item.value)">
+							<a :href="'/szukaj/' + i">{{value}} / {{i}}</a>
+						</div>
+
+					</div>
+
+					<button
+						v-if="!infoItems || (infoItems && infoItems[index] && infoItems[index].isInitiallyCollapsed)"
+						class="tify-info_toggle"
+						@click="infoItems[index].isCollapsed = !infoItems[index].isCollapsed"
+					>
+						<template v-if="!infoItems || (infoItems && infoItems[index] && infoItems[index].isCollapsed)">
+							<icon name="expand_more"/>
+							{{ 'Expand'|trans }}
+						</template>
+						<template v-else>
+							<icon name="expand_less"/>
+							{{ 'Collapse'|trans }}
+						</template>
+					</button>
+				</div>
+			</template>
+		</div>
+	</div>
 </template>
 
 <script>
+
+// <div v-bind:key="value" v-for="value in getLabels(item.value)" v-aname="value" v-html="value"/>
+
 import httpClient from '@/services/http-client';
 
 const itemMaxLines = 5;
@@ -20,7 +75,9 @@ const lineHeight = 24; // [px] value from settings.scss
 const maxCharsPerLine = 42; // value empirically determined (and checked against zooming in)
 
 export default {
-	props: ['metadata'],
+	props: [
+		'metadata',
+	],
 	data() {
 		return {
 			apiItem: null,
@@ -63,94 +120,32 @@ export default {
 
 			if (!this.apiItem) {
 				httpClient.get(apiUri).then((i) => {
+					console.log('request');
 					self.apiItem = i.data;
+					console.log(self.apiItem);
 				});
 			}
 		},
 		parseApiItem() {
 			const self = this;
 
-			const dates = {
-				d1: null,
-				d2: null,
-			};
-
 			this.apiItem.element_texts.forEach((e) => {
 				// console.log(e.element);
-
-				const remove = [41, 46, 50, 64, 65, 40, 70];
-
-				// preparing range dates
-
-				if (e.element.id === 40) dates.d1 = e.text;
-				if (e.element.id === 70) dates.d2 = e.text;
-
-				if (!remove.includes(e.element.id)) {
-					httpClient.get(e.element.url).then((el) => {
-						const m = {
-							field_id: el.data.id,
-							field_name: e.element.name_translated,
-							value: e.text,
-							order: Number(el.data.order),
-							html: `<a href='/szukaj/p:${el.data.id}/${e.text}'>${e.text}</a>`,
-							// hide: _hide,
-						};
-						self.apiMetadata.push(m);
-						self.apiMetadata.sort(
-							(a, b) => parseFloat(a.order) - parseFloat(b.order),
-						);
-						// console.log(self.apiMetadata);
-					});
-				}
+				httpClient.get(e.element.url).then((el) => {
+					const m = {
+						field_id: el.data.id,
+						field_name: e.element.name_translated,
+						value: e.text,
+						order: Number(el.data.order),
+						// hide: _hide,
+					};
+					self.apiMetadata.push(m);
+					self.apiMetadata.sort(
+						(a, b) => parseFloat(a.order) - parseFloat(b.order),
+					);
+					console.log(self.apiMetadata);
+				});
 			});
-
-			let r;
-			let rhtml;
-
-			if (dates.d1) {
-				// parsing dates
-
-				if (dates.d2) {
-					r = `${dates.d1} - ${dates.d2}`;
-					rhtml = `<a href="/szukaj/p:40/${dates.d1}">${dates.d1}</a> - 
-					<a href="/szukaj/p:70/${dates.d2}">${dates.d2}</a>`;
-				} else {
-					r = dates.d1;
-					rhtml = dates.d1;
-				}
-
-				const d = {
-					field_id: 0, // ??
-					field_name: 'Zakres',
-					value: r,
-					order: 1,
-					html: rhtml,
-				// hide: _hide,
-				};
-
-				self.apiMetadata.push(d);
-			}
-
-			// build category
-
-			const colId = this.apiItem.collection.id;
-			const colTitle = this.apiItem.collection.collection_title;
-
-			const c = {
-				field_id: 0,
-				field_name: 'Kolekcja',
-				value: this.apiItem.collection.collection_title,
-				order: 0,
-				html: `<a href='/kolekcja/${colId}'/>${colTitle}</a>`,
-				// hide: _hide,
-			};
-
-			self.apiMetadata.push(c);
-			self.apiMetadata.sort(
-				(a, b) => parseFloat(a.order) - parseFloat(b.order),
-			);
-
-			// console.log(self.apiMetadata);
 		},
 		updateInfoItems() {
 			// ????
@@ -166,9 +161,7 @@ export default {
 				const r1 = this.metadata.find((x) => x.label === 'Data');
 				const r2 = this.metadata.find((x) => x.label === 'Data2');
 				r1.value = `${r1.value} - ${r2.value}`;
-				const di = this.metadata.indexOf(
-					this.metadata.find((x) => x.label === 'Data2'),
-				);
+				const di = this.metadata.indexOf(this.metadata.find((x) => x.label === 'Data2'));
 				this.metadata.splice(di, 1);
 			}
 
@@ -201,13 +194,11 @@ export default {
 					const expectedLineNumber = values.reduce((linesSum, thisValue) => {
 						// assuming we need 1 line minimum to display each value
 						// and a fixed number of chars fits into each line
-						let nLines = Math.ceil(
-							this.stripHtml(thisValue).length / maxCharsPerLine,
-						);
+						let nLines = Math.ceil(this.stripHtml(thisValue).length / maxCharsPerLine);
 						if (nLines < 1) nLines = 1;
 						return linesSum + nLines;
 					}, 0);
-					const limitHeight = expectedLineNumber > itemMaxLines;
+					const limitHeight = (expectedLineNumber > itemMaxLines);
 					const infoItem = {
 						isCollapsed: limitHeight,
 						isInitiallyCollapsed: limitHeight,
